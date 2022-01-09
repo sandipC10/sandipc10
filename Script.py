512545ff0dc878f8423dc2f65e85b2df1556dbb4
Heroku077 <94363128+Heroku077@users.noreply.github.com>
2022-01-09 18:27:16 +0530

    Initial commit

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- IRON MAN Bot is a open source project. 
+- IRON MAN Bot is *NOT* a open source project. 
 - Source - https://t.me/mr_anshu_07  
 
 <b>DEVS:</b>
