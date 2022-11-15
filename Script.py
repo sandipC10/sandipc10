539a163e83ef40ce29f1b5e4e1492465c0af612b
sandipC10 <110212502+sandipC10@users.noreply.github.com>
2022-11-15 15:00:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,14 +3,14 @@ class script(object):
 𝙒𝙚𝙡𝙘𝙤𝙢𝙚 𝙏𝙤 <a href=https://t.me/{}>{}</a>, 𝙈𝙮𝙩𝙝𝙤𝙡𝙤𝙜𝙮 𝙎𝙚𝙧𝙞𝙖𝙡 𝘽𝙤𝙩. 𝙃𝙚𝙧𝙚 𝙔𝙤𝙪 𝘾𝙖𝙣 𝙁𝙞𝙣𝙙 𝙈𝙮𝙩𝙝𝙤𝙡𝙤𝙜𝙮 𝙎𝙚𝙧𝙞𝙖𝙡 𝙇𝙞𝙠𝙚 𝕽α∂нαкяιѕнη, 𝙈𝙪𝙨𝙩 𝙅𝙤𝙞𝙣 𝙍𝙠 𝙏𝙤 𝙐𝙨𝙚 𝙏𝙝𝙞𝙨 𝘽𝙤𝙩 
 """
     HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 Devoloper & Manage BY @Sandipchavan10."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-☞ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Sandipchavan10>Sαɳԃιρ</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-☞ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-☞ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: Searching......🔍
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 Devoloper & Manage BY @𝘚𝘢𝘯𝘥𝘪𝘱𝘤𝘩𝘢𝘷𝘢𝘯10."""
+    ABOUT_TXT = """♟ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
+♟ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Sandipchavan10>Sαɳԃιρ</a>
+♟ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+♟ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+♟ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+♟ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠...
+♟ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ Beta ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - 𝕽α∂нαкяιѕнη Bot is Not a open source Project. 
 - Source - https://t.me/Sandipchavan10  
@@ -93,10 +93,10 @@ This module only works for my admins
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-☞ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-✯ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-☞ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-✯ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+♟ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+♟ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+♟ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+♟ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
