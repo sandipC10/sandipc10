6b1e3d46c1e50a1f694274a97d0b06e35f39f577
sandipC10 <110212502+sandipC10@users.noreply.github.com>
2022-11-20 10:51:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sandipC10/sandip100-LH.git /sandip100-LH
+  git clone https://github.com/sandipC10/sandipc10.git /sandipc10
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /sandip100-LH
+  git clone $UPSTREAM_REPO /sandipc10
 fi
 cd sandip100-LH
 pip3 install -U -r requirements.txt
